5bd79944ac09312dcbbfcc26bbe9e71a244650cb
Scott Campbell <stc9486@gmail.com>
2020-01-29 20:22:38 -0800

    safety save

--- app.js
+++ app.js
@@ -6,30 +6,19 @@ const productsList = products.slice();
 let selectedProductCount; 
 let totalViews;
 let totalSets;
-
-
 const initiateState = () => {
     selectedProductCount = [];
     totalViews = [];
-    totalSets = 0;
+    totalSets = 0; 
 };
-
 initiateState();
 
-/// ---> SECOND : What will the user do to interact and modify these variables and update the state? AND, what does the site require for such an intervention?
-// 1. Display content
-    // need a number randomizer function that will generate nums that we can use as indicies to identify and return sets of items at random
-      // must return 3 dissimlar products 
-      
-  
-    //randomizer
 const randomProduct = (productsList) => {
     const index = Math.floor(Math.random() * productsList.length);
     const randomProduct = productsList[index];
     return randomProduct;
 }
 
-  //function to return 3 dissimlar products 
 const returnThreeProducts = () => {
     let productOne = randomProduct(productsList);
     let productTwo = randomProduct(productsList);
@@ -37,13 +26,12 @@ const returnThreeProducts = () => {
         productTwo = randomProduct(productsList)
     }
     let productThree = randomProduct(productsList);
-    while (productThree === productTwo || productThree === productOne) {
+    while (productThree.id === productTwo.id || productThree.id === productOne.id) {
         productThree = randomProduct(productsList);
+        const myThree = [productOne, productTwo, productThree];
+        return myThree;
     }
-/// 2: equipped with those products, now manipulate the DOM, rendering the content
 
-   /// these are all NEW variables, DOM to JS
-    // id's
     const radioOneButton = document.getElementById('radio-1');
     const radioTwoButton = document.getElementById('radio-2');
     const radioThreeButton = document.getElementById('product-3') ;
@@ -59,56 +47,47 @@ const returnThreeProducts = () => {
     const radioThreeImage = document.getElementById('radio-3-image');
 
 
-    //Assign appropriate properties to DOM-grabbing-variables
-    //button
-        // id's --> values
     radioOneButton.value = productOne.id;
     radioTwoButton .value = productTwo.id;
     radioThreeButton.value = productThree.id;
 
-    //text.content
-        // h3's to names
     productOneText.textContent = productOne.name;
     productTwoText.textContent = productTwo.name;
     productThreeText.textContent = productThree.name;
 
-    //img
-        // assign the DOM variable's src to the iamge propert of generated product because 
+
     radioOneImage.src = productOne.image;
     radioTwoImage.src = productTwo.image;
     radioThreeImage.src = productThree.image;
+
+    const thisSet = [productOne.id, productTwo.id, productThree.id];
+    totalViews.push(thisSet);
     
 };
 
 
-// 3. Event Listener
-
-const form = document.querySelector('form')
-//this is creating a variable that is assigned to the value of the form and its contents
+const form = document.querySelector('form');
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     
-    console.log('submit is live');
-    
-    // conditions the circumsances of survey rendering
     if (totalSets >= 25) {
         displayResults();
     } else {
         totalSets += 1;
-        console.log('total sets = ' + totalSets);
+            console.log('total sets = ' + totalSets);
     };
 
     const formData = new FormData(form);
-    //this is taking in the form var, so the value of formData(var) is the data of the form itself upon event.
+
 
     const selectedProductId = formData.get('product');
-    console.log('selected products = ' + selectedProductId);
-    //this is assigning the value of the selected product to the var
+        console.log('selected products = ' + selectedProductId);
+  
  
 
     const productExistCheck = findById(selectedProductCount, selectedProductId);
-    console.log(productExistCheck);
+    
 
     //conditional to either push that product into array as new object or add it to existing object
     if (productExistCheck) {
@@ -120,14 +99,16 @@ form.addEventListener("submit", (e) => {
             votes: 1,
         });
         
-console.log(selectedProductCount)
+    console.log(productExistCheck);
+    console.log(selectedProductCount);
     } 
     returnThreeProducts();
+  
 });
 
-function reset() {
-    initiateState();
-}
+//function reset() {
+    //initiateState();
+//}
 
     
 
